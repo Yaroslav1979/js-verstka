ce228a6f4b1555ce3cb7bf65a28b394f5978ebdb
Yaroslav1979 <yarico.rv@gmail.com>
2024-03-12 20:51:52 +0200

    finished

--- src/container/community/index.js
+++ src/container/community/index.js
@@ -16,10 +16,15 @@ page.append(title)
 
 //---------------------------------------------------------------
 
-const createKnowledgeBase = () => {
+const createPage = () => {
+
     const tabsContainer = createElement('div', 'tabs');
-    const knowledgeTabButton = createElement('button', 'tab', 'База знань');
-    const infoTabButton = createElement('button', 'tab', 'Інформація');
+    const knowledgeTab = createElement('button', 'tab', 'База знань');
+    const infoTab = createElement('button', 'tab', 'Інформація');
+    
+    tabsContainer.append(knowledgeTab);
+    tabsContainer.append(infoTab);
+
     const mainContent = createElement('main', 'post__list');
 
     const heroImage = createElement('img', 'hero');
@@ -29,131 +34,30 @@ const createKnowledgeBase = () => {
     const subtitle = createElement('h2', 'subtitle', 'Що таке база знань?');
 
     const postInfo = createElement('p', 'post__info');
-    postInfo.innerHTML = 'База знаний — база данных, содержащая правила ' +
-        'вывода и информацию о человеческом опыте и ' +
-        'знаниях в некоторой предметной области. ' +
-        'В самообучающихся системах база знаний также ' +
-        'содержит информацию, являющуюся результатом решения ' +
-        'предыдущих задач.';
+    postInfo.innerHTML = 'База знаний — база данных, содержащая правила вывода и информацию о человеческом опыте и знаниях в некоторой предметной области. В самообучающихся системах база знаний также содержит информацию, являющуюся результатом решения предыдущих задач.';
 
-    const telegramLink = createElement('a', 'link', 'Перейти до ком\'юніті у Телеграм');
-    telegramLink.href = '#';
+    const link = createElement('a', 'link', "Перейти до ком'юніті у Телеграм");
+    link.href = '#';
 
-    // Додаємо створені елементи до відповідних контейнерів
-    tabsContainer.appendChild(knowledgeTabButton);
-    tabsContainer.appendChild(infoTabButton);
+  
+    mainContent.append(heroImage);
+    mainContent.append(subtitle);
+    mainContent.append(postInfo);
+    mainContent.append(link);
 
-    mainContent.appendChild(heroImage);
-    mainContent.appendChild(subtitle);
-    mainContent.appendChild(postInfo);
-    mainContent.appendChild(telegramLink);
-
-    // Створюємо об'єкт для повернення контейнера
-    const knowledgeBase = {
+    
+    const mainBlock = {
         tabsContainer,
         mainContent,
     };
 
-    return knowledgeBase;
+    return mainBlock;
 };
-const knowledgeBase = createKnowledgeBase();
 
-// Додаємо контейнери до сторінки
-page.appendChild(knowledgeBase.tabsContainer);
-page.appendChild(knowledgeBase.mainContent);
-//-------------------------------------
+const mainBlock = createPage();
 
+page.append(mainBlock.tabsContainer);
+page.append(mainBlock.mainContent);
 
-
-
-
-
-
-// const TAB_LIST = [
-//     {
-//         text: 'База знань', 
-//         id: 1
-//     },
-//     {
-//         text: 'Інформація',
-//         id: 2
-//     },
-// ]
-
-// const createTabs = () => {
-//    const tabs = createElement('div', 'tabs')
-// }
-
-
-
-
-// const POST_LIST = [
-//     {
-//         category: [
-//             { text: 'Важливо', id: 1 },
-//             { text: 'Нова', id: 2 },
-//         ],
-//         info: 'До біса планувальник, наймаємо дизайнера і готуємося до презентації, як Джобс',
-//         date: '25.01',
-//         viewed: false
-//     },
-//     {
-//         category: [
-//             { text: 'Нова', id: 2 },
-//         ],
-//         info: 'Ми хотіли щоб у цьому чаті було близько 150 людей щоб зробити якісний пак самопрезентацій.',
-//         date: '24.01',
-//         viewed: true
-//     },
-// ]
-
-// const createPost = () => {
-//  const postList = createElement('main', 'post__list')
-
-// POST_LIST.forEach((postData) => {
-//     const post = createElement(
-//         'div',
-//         postData.viewed 
-//         ? 'post button post--viewed'
-//         : 'post button',
-//     )
-
-//     const postHeader = createElement('div', 'post__header')
-
-//     const categoryList = createElement(
-//         'div',
-//         'post__category-list',
-//     )
-
-//     postData.category.forEach((category) => {
-//         const categorySpan = createElement(
-//             'span',
-//             `post__category post__category--${category.id}`,
-//             category.text,
-//         )
-//         categoryList.append(categorySpan)
-//     })
-
-//     const dateSpan = createElement(
-//         'span',
-//         'post__date',
-//         postData.date,
-//     )
-
-//     postHeader.append(categoryList, dateSpan)
-
-//     const infoParagraph = createElement(
-//         'p',
-//         'post__info',
-//         postData.info,
-//     )
-//     post.append(postHeader, infoParagraph)
-
-//     postList.append(post)
-//     })
-// return postList
-// }
-// const post = createPost()
-// page.append(post)
     
     
